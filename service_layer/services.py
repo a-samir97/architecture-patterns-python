2e8045f128c40d1faebe6429dd39f20bb344f45b
Ahmed Samir <a.samir9710@gmail.com>
2024-07-09 00:54:39 +0300

    chapter 5: TDD and service layer unit test with domain model unit tests

--- service_layer/services.py
+++ service_layer/services.py
@@ -9,11 +9,16 @@ def is_valid_sku(sku, batches):
     return sku in {b.sku for b in batches}
 
 
-def allocate(line, repo, session):
+def add_batch(ref, sku, qty, eta, repo, session):
+    repo.add(model.Batch(ref, sku, qty, eta))
+    session.commit()
+
+
+def allocate(orderid, sku, qty, repo, session):
     batches = repo.list()
 
-    if not is_valid_sku(line.sku, batches):
-        raise InvalidSku(f"Invalid sku {line.sku}")
+    if not is_valid_sku(sku, batches):
+        raise InvalidSku(f"Invalid sku {sku}")
 
     batchref = model.allocate(line, batches)
     session.commit()
